b26e50bc999dc9a5f3533c00df0b0832c5588a9c
october-x <2290512627@qq.com>
2023-03-07 23:02:30 +0800

    [添加]监控图片加载方法

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-export const debounce = function (fn, duration:number) {
+export const debounce = function (fn, duration: number) {
   let timerId: any = null;
   // @ts-ignore
   return (...args) => {
@@ -11,17 +11,48 @@ export const debounce = function (fn, duration:number) {
   };
 };
 
-
 // @ts-ignore
-export function curry(fn,...args){
-  if(args.length >= fn.length){
+export function curry(fn, ...args) {
+  if (args.length >= fn.length) {
     // @ts-ignore
-      return fn.apply(this,args);
+    return fn.apply(this, args);
   }
   // @ts-ignore
-  return function(...vals){
-      let totalArgs = args.concat(vals)
-      // @ts-ignore
-      return curry.call(this,fn,...totalArgs)
+  return function (...vals) {
+    let totalArgs = args.concat(vals);
+    // @ts-ignore
+    return curry.call(this, fn, ...totalArgs);
+  };
+}
+
+export function imageLoaded(imageUrl: string, callback: () => void) {
+  let img = new Image();
+  function imageLoaded() {
+    callback();
+  }
+  img.onload = imageLoaded;
+  img.onerror = imageLoaded;
+  img.src = imageUrl;
+}
+
+type imagesItem = {
+  src: string;
+};
+export function allImagesLoaded(images: imagesItem[], callback: () => void) {
+  let loaded = 0;
+  let imgCount = images.length;
+
+  function imageLoaded() {
+    loaded++;
+    if (loaded === imgCount) {
+      callback();
+    }
+  }
+
+  for (let i = 0; i < imgCount; i++) {
+    let img = new Image();
+    img.onload = imageLoaded;
+    img.onerror = imageLoaded;
+    img.src = images[i].src;
   }
 }
